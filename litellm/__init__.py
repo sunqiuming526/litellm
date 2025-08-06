a6f00afd53b50a6b2bf07a7c603fd724f1e334e5
Andrew Sun <andrew.sun01@sap.com>
2025-08-06 11:50:27 +0800

    import SAPConfig back

--- litellm/__init__.py
+++ litellm/__init__.py
@@ -979,6 +979,8 @@ ALL_LITELLM_RESPONSE_TYPES = [
 
 from .llms.bytez.chat.transformation import BytezChatConfig
 from .llms.custom_llm import CustomLLM
+from .llms.sap.chat.transformation import SAPChatConfig
+from .llms.sap.chat.converse_transformation import SAPConverseConfig
 from .llms.bedrock.chat.converse_transformation import AmazonConverseConfig
 from .llms.openai_like.chat.handler import OpenAILikeChatConfig
 from .llms.aiohttp_openai.chat.transformation import AiohttpOpenAIChatConfig
